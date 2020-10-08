3bd4a364f04ee59496a374c88282ef20d370aca4
STEPin105183 <mishramanisha.423@gmail.com>
2020-10-08 18:05:26 +0530

    changes

--- QuizzineFinal.c
+++ QuizzineFinal.c
@@ -6,7 +6,7 @@
 #include<time.h>
 #include<dos.h>
 
-void displayscore()
+/*void displayscore()
 {
 	char name[20];
 	float s;
@@ -19,6 +19,11 @@ void displayscore()
 	printf("\n\n\t\t ");
 	fclose(f);
 	getch();
+}*/
+void displayscore(){
+	char name[20];
+	int score;
+	printf("%c has secured the highest score %d",name,score);
 }
 void help()
 {
@@ -40,7 +45,7 @@ int main(){
     int cgry1; int cgry2; int cgry3;
     int mopt;
     int opt;
-    int q1,q2,q3,q4,q5,q6,q7,q8,q9;
+    int q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17,q18;
     MAINQ:
     system("cls");
     SetConsoleTitle("\n\t\t QUIZZINE: A Quiz Game \n\n ");
@@ -333,16 +338,16 @@ cgry2:
     printf("Welcome to WARM-UP round ( Round-1 )\n\n");
 	printf("[Verbal & Logical Reasoning]\n\n");
 	
-	printf("The total of the ages of Amar, Akbar and anthony is 80 years. What was the total of their ages 3years ago?\n");
+	printf("Q7 : The total of the ages of Amar, Akbar and anthony is 80 years. What was the total of their ages 3years ago?\n");
 	printf("1 > 71 2 > 72 3 > 74 4 > 77\n");
 	printf("Enter Correct Option : ");
 	scanf("%d",&q1);
-	if( q1 > 4){
+	if( q7 > 4){
 		system("cls");
 		goto cgry2;
 	}
 
-    if (q1 == 3){
+    if (q7 == 3){
     	score1 =10;
     	printf("\t\t\t\t\t\t\t Score = %d",score1);
     	printf("\n\t\t\t\t\t\t\t Correct Answer");
@@ -356,16 +361,16 @@ cgry2:
     	printf("\n\t\t\t\t\t\t\t Wrong Answer");		
 	}
 
-	printf("\nThe metal whose salts are sensitive to light is ?\n");
-	printf("1 > Zinc 2 > Silver 3 > Copper 4 > Aluminum\n");
+	printf("\n Q8 : Which of the word which best expresses the meaning of the given word EMBEZZLE. ?\n");
+	printf("1 > Misappropriate 2 > Balance 3 > Remunerate 4 > Clear\n");
 	printf("Enter Correct Option : ");
 	scanf("%d",&q2);
-	if( q2 > 4){
+	if( q8 > 4){
 		system("cls");
 		goto cgry2;
 	}
 
-    if (q2 == 2){
+    if (q8 == 2){
     	score1 = score1 +10;
     	printf("\t\t\t\t\t\t\t Score = %d",score1);
     	printf("\n\t\t\t\t\t\t\t Correct Answer");
@@ -379,17 +384,16 @@ cgry2:
 	}
 	
 	
-	
-	printf("\nCountry that has the highest in Barley Production ?");
-	printf("\n1 > china 2 > Pakistan 3 > Russia 4 > france");
+	printf("\n Q9 : Choose the correct meaning of proverb/idiom- To keeps one's temper");
+	printf("\n1 > To become hungry 2 > To be in good mood 3 > To preserve ones energy 4 > To be aloof from");
 	printf("\nEnter Correct option : ");
-	scanf("%d",&q3);
-		if(q3 > 4)
+	scanf("%d",&q9);
+		if(q9 > 4)
 	{
 		system("cls");
 		goto cgry2;
 	}
-	if(q3==3)
+	if(q9==3)
 	{
 		score1 = score1+10;
 		printf("\t\t\t\t\t\t\t Score = %d",score1);
@@ -445,15 +449,15 @@ cgry2:
 	printf("[Verbal & Logical Reasoning]\n\n");
 	printf("Welcome to Challenging round ( Round-2 )\n\n");
 	
-	printf("\nWhat is the product of all the numbers in the dial of a telephone ?");
+	printf("\n Q10 : What is the product of all the numbers in the dial of a telephone ?");
 	printf("\n 1 > 1,58,480 2 > 1,59,450 3 > 1.59.480 4 > None of these");
 	printf("\nEnter Correct Option : ");
-	scanf("%d",&q4);
-	if(q4 > 4){
+	scanf("%d",&q10);
+	if(q10 > 4){
 		system("cls");
 		goto Round4;
 	}
-	if(q4 == 1){
+	if(q10 == 1){
 		score2=10;
 		printf("\t\t\t\t\t\t\t Score = %d",score2);
 		printf("\n\t\t\t\t\t\t\t Correct Answer");
@@ -466,15 +470,15 @@ cgry2:
 		printf("\n\t\t\t\t\t\t\tWrong Answer");
 	}
 	
-	printf("\nWhat is the Capital of Pakistan");
-	printf("\n 1 > Lahore 2 > Quetta 3 > Islamabad 4 >Karachi");
+	printf("\n Q11 : Look at this series: 5.2, 4.8, 4.4, 4, ... What number should come next?");
+	printf("\n 1 > 3 2 > 3.3 3 > 3.5 4 > 3.6");
 	printf("\nEnter Correct Option : ");
-	scanf("%d",&q5);
-	if(q5 > 4){
+	scanf("%d",&q11);
+	if(q11 > 4){
 		system("cls");
 		goto Round2;
 	}
-	if(q5 == 3){
+	if(q11 == 3){
 		score2=score2+10;
 		printf("\t\t\t\t\t\t\t Score = %d",score2);
 		printf("\n\t\t\t\t\t\t\t Correct Answer");
@@ -489,16 +493,16 @@ cgry2:
 	
 	
 	
-	printf("\nMicrosoft was founded by?");
-	printf("\n 1 > billgates 2 > jeff 3 > steve 4 > mark ");
+	printf("\n Q12 : Select the pair which has the same relationship as- DIVA:OPERA");
+	printf("\n 1 > producer:theatre 2 > director:drama 3 > conductor:bus 4 > thespian:play ");
 	printf("\nEnter Correct Option : ");
-	scanf("%d",&q6);
-	if(q6 > 4){
+	scanf("%d",&q12);
+	if(q12 > 4){
 		system("cls");
 		goto Round2;
 	}
 	
-	if(q6 == 1){
+	if(q12 == 1){
 		score2=score2+10;
 		printf("\t\t\t\t\t\t\t Score = %d",score2);
 		printf("\n\t\t\t\t\t\t\t Correct Answer");
@@ -518,16 +522,16 @@ cgry2:
     printf("Welcome to WARM-UP round ( Round-1 )\n\n");
 	printf("[Quantitative Aptitude]\n\n");
 	
-	printf("Find the greatest number that will divide 43, 91 and 183 so as to leave the same remainder in each case.\n");
+	printf(" Q13 : Find the greatest number that will divide 43, 91 and 183 so as to leave the same remainder in each case.\n");
 	printf("1 > 4 > 7 3 > 9 4 > 13\n");
 	printf("Enter Correct Option : ");
-	scanf("%d",&q1);
-	if( q1 > 4){
+	scanf("%d",&q13);
+	if( q13 > 4){
 		system("cls");
 		goto cgry3;
 	}
 
-    if (q1 == 3){
+    if (q13 == 3){
     	score1 =10;
     	printf("\t\t\t\t\t\t\t Score = %d",score1);
     	printf("\n\t\t\t\t\t\t\t Correct Answer");
@@ -541,16 +545,16 @@ cgry2:
     	printf("\n\t\t\t\t\t\t\t Wrong Answer");		
 	}
 
-	printf("\nThe metal whose salts are sensitive to light is ?\n");
-	printf("1 > Zinc 2 > Silver 3 > Copper 4 > Aluminum\n");
+	printf("\n Q14 : In the first 10 overs of a cricket game, the run rate was only 3.2. What should be the run rate in the remaining 40 overs to reach the target of 282 runs?\n");
+	printf("1 > 6.25 2 > 6.5 3 > 6.75 4 > 7\n");
 	printf("Enter Correct Option : ");
-	scanf("%d",&q2);
-	if( q2 > 4){
+	scanf("%d",&q14);
+	if( q14 > 4){
 		system("cls");
 		goto cgry3;
 	}
 
-    if (q2 == 2){
+    if (q14 == 2){
     	score1 = score1 +10;
     	printf("\t\t\t\t\t\t\t Score = %d",score1);
     	printf("\n\t\t\t\t\t\t\t Correct Answer");
@@ -565,16 +569,16 @@ cgry2:
 	
 	
 	
-	printf("\nCountry that has the highest in Barley Production ?");
-	printf("\n1 > china 2 > Pakistan 3 > Russia 4 > france");
+	printf("\nQ15 : Two numbers are respectively 20% and 50% more than a third number. The ratio of the two numbers is:");
+	printf("\n1 > 2:5 2 > 3:5 3 > 4:5 4 > 6:7");
 	printf("\nEnter Correct option : ");
-	scanf("%d",&q3);
-		if(q3 > 4)
+	scanf("%d",&q15);
+		if(q15 > 4)
 	{
 		system("cls");
 		goto cgry3;
 	}
-	if(q3==3)
+	if(q15==3)
 	{
 		score1 = score1 +10;
 		printf("\t\t\t\t\t\t\t Score = %d",score1);
@@ -630,15 +634,15 @@ cgry2:
 	printf("[Quantitative Aptitude]\n\n");
 	printf("Welcome to Challenging round ( Round-2 )\n\n");
 	
-	printf("\nFirst China War was fought between");
+	printf("\nQ16 : First China War was fought between");
 	printf("\n 1 > China and Britain 2 > China and France. 3 > China and Egypt 4 >China and Greek");
 	printf("\nEnter Correct Option : ");
-	scanf("%d",&q4);
-	if(q4 > 4){
+	scanf("%d",&q16);
+	if(q16 > 4){
 		system("cls");
 		goto Round6;
 	}
-	if(q4 == 1){
+	if(q16 == 1){
 		score2=10;
 		printf("\t\t\t\t\t\t\t Score = %d",score2);
 		printf("\n\t\t\t\t\t\t\t Correct Answer");
@@ -651,15 +655,15 @@ cgry2:
 		printf("\n\t\t\t\t\t\t\tWrong Answer");
 	}
 	
-	printf("\nWhat is the Capital of Pakistan");
-	printf("\n 1 > Lahore 2 > Quetta 3 > Islamabad 4 >Karachi");
+	printf("\nQ17 : Out of 7 consonants and 4 vowels, how many words of 3 consonants and 2 vowels can be formed?");
+	printf("\n 1 > 210 2 > 1050 3 > 25200 4 > 21400 \n");
 	printf("\nEnter Correct Option : ");
-	scanf("%d",&q5);
-	if(q5 > 4){
+	scanf("%d",&q17);
+	if(q17 > 4){
 		system("cls");
 		goto Round2;
 	}
-	if(q5 == 3){
+	if(q17 == 3){
 		score2=score2+10;
 		printf("\t\t\t\t\t\t\t Score = %d",score2);
 		printf("\n\t\t\t\t\t\t\t Correct Answer");
@@ -674,16 +678,16 @@ cgry2:
 	
 	
 	
-	printf("\nMicrosoft was founded by?");
-	printf("\n 1 > billgates 2 > jeff 3 > steve 4 > mark ");
+	printf("\nQ18 : What is the probability of getting a sum 9 from two throws of a dice?");
+	printf("\n 1 > 1/6 2 > 1/8 3 > 1/9 4 > 1/12 ");
 	printf("\nEnter Correct Option : ");
-	scanf("%d",&q6);
-	if(q6 > 4){
+	scanf("%d",&q18);
+	if(q18 > 4){
 		system("cls");
 		goto Round2;
 	}
 	
-	if(q6 == 1){
+	if(q18 == 1){
 		score2=score2+10;
 		printf("\t\t\t\t\t\t\t Score = %d",score2);
 		printf("\n\t\t\t\t\t\t\t Correct Answer");
